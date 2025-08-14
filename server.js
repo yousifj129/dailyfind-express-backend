0593440cb4e11fef1afdb1ab4ddaf03f807185bb
Yaqoob <yaqoob.buhejji@gmail.com>
2025-08-14 16:53:16 +0300

    Working on the server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require("express")
+const mongoose = require ('mongoose')
+const logger = require('morgan')
+const cors = require ('cors')
+
+
+app.listen( 3000, ()=>{
+    console.log('The website working ')
+}
+
+)
